b6571ff124a925b798821972c042014597e19bcc
Egor Sysoev <m4mindscape@gmail.com>
2017-07-28 13:15:12 +0500

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,16 @@
-package ru.job4j;
 
+package ru.job4j;
+/**
+* Class Класс для вычисления арифметических операций
+*
+* @author Egor Sysoev
+* @since 28.08.2017
+* @version 1
+*/
 class Calculate {
+	/**
+	*
+	*/
 	public static void main (String [] args) {
 		System.out.println("Hello World");
 	}
